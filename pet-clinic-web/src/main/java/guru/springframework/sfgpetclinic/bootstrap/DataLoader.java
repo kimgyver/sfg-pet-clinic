b9085bd6703956a0eb12a667949b128687638742
Jinyoung Kim <jinyoung777@gmail.com>
2020-05-09 20:18:22 +1200

    list all owners

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -27,7 +27,7 @@ public class DataLoader implements CommandLineRunner {
 
         Owner owner2 = new Owner();
         owner2.setId(2L);
-        owner2.setLastName("Fionna");
+        owner2.setFirstName("Fionna");
         owner2.setLastName("Glenanne");
         ownerService.save(owner2);
 
